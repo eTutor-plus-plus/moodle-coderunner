3c9e0ca2ef086dc4214931b96ecef640449cf699
kevschuetz <kevin.hotz@gmx.at>
2023-08-29 14:49:13 +0200

    changed jobe configuration

--- jobeinabox/entrypoint.sh
+++ jobeinabox/entrypoint.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Add dispatcher host and dispatcher port to the apache environment variables
+# Remove old entries
+#sed -i '/export DISPATCHER_HOST=/d' /etc/apache2/envvars
+#sed -i '/export DISPATCHER_PORT=/d' /etc/apache2/envvars
+    
+# Add the current values
+#echo "export DISPATCHER_HOST=$DISPATCHER_HOST" >> /etc/apache2/envvars
+#echo "export DISPATCHER_PORT=$DISPATCHER_PORT" >> /etc/apache2/envvars
+
+# Remove old entries
+#sed -i '/DISPATCHER_HOST=/d' /etc/environment
+#sed -i '/DISPATCHER_PORT=/d' /etc/environment
+#    
+# Add the current values
+#echo "DISPATCHER_HOST=$DISPATCHER_HOST" >> /etc/environment
+#echo "DISPATCHER_PORT=$DISPATCHER_PORT" >> /etc/environment
+
+
+# Update number of simultaneous jobs
+perl -pi -e "s/\$config\['jobe_max_users'\] = \d+;/\$config\['jobe_max_users'\] = ${SIM_JOBE_JOBS};/" /var/www/html/jobe/application/config/config.php
+
 # Start the Apache web server (in the background)
 /usr/sbin/apache2ctl -D FOREGROUND &
 
